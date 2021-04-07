0432be5a6b2b6aeae8a33e3cb6c006d5b0b9769e
Jordan Jamali <jordanjjamali@gmail.com>
2021-04-07 01:47:55 -0400

    Add smiting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -10,8 +11,11 @@ intents.members = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
 
+con = sqlite3.connect('blacklist.db')
+cur = con.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS blacklist (userid text)')
+con.commit()
 
-# client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -35,15 +39,26 @@ async def on_message(message):
     if message.content.startswith('!banish'):
         await update_all_members_in_guild(message.guild)
 
+    if message.content.startswith('!smite'):
+        if message.author.guild_permissions.administrator:
+            member1 = message.author.guild.get_member(message.mentions[0].id)
+            cur.execute("INSERT INTO blacklist VALUES (?)", (member1.id,))
+            con.commit()
+            await force_update_member(member1)
+
 
 @client.event
 async def on_member_update(before: discord.Member, after: discord.Member):
 
-    await update_member(after)
+    if after.display_name[0] == '☹':
+        return
+    if blacklisted(after):
+        await force_update_member(after)
+    else:
+        await update_member(after)
 
 
 async def update_member(member: discord.Member):
-
     new_nick = member.display_name
     my_list = list(new_nick)
 
@@ -52,5 +67,26 @@ async def update_member(member: discord.Member):
 
         new_nick = "".join(my_list)
         await member.edit(nick=new_nick)
+
+
+async def force_update_member(member: discord.Member):
+
+    new_nick = member.display_name
+    my_list = list(new_nick)
+    my_list[0] = '☹'
+
+    new_nick = "".join(my_list)
+
+    await member.edit(nick=new_nick)
+
+
+def blacklisted(member: discord.Member):
+    cur.execute("SELECT EXISTS(SELECT 1 FROM blacklist WHERE userid=?)", (member.id,))
+
+    print(member.display_name)
+    if cur.fetchone()[0]:
+        return True
+    return False
+
 
 client.run(os.getenv('TOKEN'))
